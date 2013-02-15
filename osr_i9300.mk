644fb3d6172162a9e2645ee018d52688df4d7b27
superatmel <superatmel@gmail.com>
2013-02-15 10:34:56 +0100

    Add control languages
    Change-Id: I0cb3780517b419b9f74b52b1b9853b74093cb90d

--- osr_i9300.mk
+++ osr_i9300.mk
@@ -16,7 +16,6 @@ $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
 # Add Google apps
 $(call inherit-product, vendor/google/minimal.mk)
-#$(call inherit-product, vendor/google/products/gallery_support.mk)
 #$(call inherit-product, vendor/google/products/ears_support.mk)
 # $(call inherit-product, vendor/google/products/tts_support.mk)
 $(call inherit-product, vendor/google/products/maps_support.mk)
@@ -26,6 +25,21 @@ $(call inherit-product, vendor/google/products/youtube_support.mk)
 
 # Addons personal (call files from vendor/osr/prebuilt/app)
 #$(call inherit-product, vendor/osr/addons/superatmel.mk)
+
+# Get a list of languages.
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+# Get the TTS language packs
+#$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
+
+#Languages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_small.mk)
+
+# This file simply inherits from all the know language packs.
+$(call inherit-product, external/svox/pico/lang/PicoLangDeDeInSystem.mk)
+$(call inherit-product, external/svox/pico/lang/PicoLangEnGBInSystem.mk)
+$(call inherit-product, external/svox/pico/lang/PicoLangEnUsInSystem.mk)
+$(call inherit-product, external/svox/pico/lang/PicoLangEsEsInSystem.mk)
+
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
