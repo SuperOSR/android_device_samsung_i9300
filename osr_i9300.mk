378fb55e0f44868f8af2adffde8ce69cec7198c5
superatmel <superatmel@gmail.com>
2013-04-18 10:46:46 +0200

    Addos
    Change-Id: I98c95aaa333b5237d2ee3e2781ee56d86d161444

--- osr_i9300.mk
+++ osr_i9300.mk
@@ -24,7 +24,7 @@ $(call inherit-product, vendor/google/products/facelock_support.mk)
 $(call inherit-product, vendor/google/products/youtube_support.mk)
 
 # Addons personal (call files from vendor/osr/prebuilt/app)
-#$(call inherit-product, vendor/osr/addons/superatmel.mk)
+$(call inherit-product, vendor/osr/addons/superatmel.mk)
 
 # Get a list of languages.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
